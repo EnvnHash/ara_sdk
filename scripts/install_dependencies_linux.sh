0fb84673a5acb8d03bfafb459d4f70446de417ce
sven <hahne@zeitkunst.eu>
2025-04-21 10:18:08 -0400

    dependencies checkout script for linux workflow

--- scripts/install_dependencies_linux.sh
+++ scripts/install_dependencies_linux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get install libfreeimage-dev libglew-dev libglfw3-dev libassimp-dev libpugixml-dev libglm-dev libgtk-3-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libpostproc-dev libswsample-dev libswscale-dev
+sudo apt-get install libfreeimage-dev libglew-dev libglfw3-dev libassimp-dev libpugixml-dev libglm-dev libgtk-3-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libpostproc-dev libswsample-dev libswscale-dev
