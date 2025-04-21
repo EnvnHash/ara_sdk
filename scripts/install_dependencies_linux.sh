38f6098fd7456964dd5aaf013b7ad067e96c5dcb
sven <hahne@zeitkunst.eu>
2025-04-21 10:22:49 -0400

    linux cmake build workflow

--- scripts/install_dependencies_linux.sh
+++ scripts/install_dependencies_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y libfreeimage-dev libglew-dev libglfw3-dev libassimp-dev libglm-dev libgtk-3-dev
+sudo apt-get install -y libfreeimage-dev libglew-dev libglfw3-dev libassimp-dev libglm-dev libgtk-3-dev libxxf86vm-dev
 #  libswscale-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libpostproc-dev libswsample-dev libpugixml-dev
