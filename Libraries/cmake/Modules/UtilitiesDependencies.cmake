eb7aebbaa47e2daef01ed98a22498073ed36f279
sven <hahne@zeitkunst.eu>
2025-08-18 16:06:57 -0400

    Dwmapi.lib cleanup

--- Libraries/cmake/Modules/UtilitiesDependencies.cmake
+++ Libraries/cmake/Modules/UtilitiesDependencies.cmake
@@ -2,10 +2,10 @@ include(${ARA_SDK_SOURCE_DIR}/Libraries/cmake/Modules/AraSdkMacros.cmake)
 
 if(WIN32)
     if (ARA_USE_CURL)
-        set(UTILITIES_LIBS wsock32 ws2_32 user32 iphlpapi Strmiids.lib Dwmapi.lib winmm.lib ${ARA_SDK_SOURCE_DIR}/Libraries/third_party/curl/lib/libcurl.dll.a)
+        set(UTILITIES_LIBS wsock32 ws2_32 user32 iphlpapi Strmiids.lib winmm.lib ${ARA_SDK_SOURCE_DIR}/Libraries/third_party/curl/lib/libcurl.dll.a)
         append_unique(ara_sdk_BINARIES ${ARA_SDK_SOURCE_DIR}/Libraries/third_party/curl/bin/libcurl-x64.dll)
     else()
-        set(UTILITIES_LIBS wsock32 ws2_32 user32 iphlpapi Strmiids.lib Dwmapi.lib winmm.lib )
+        set(UTILITIES_LIBS wsock32 ws2_32 user32 iphlpapi Strmiids.lib winmm.lib )
     endif()
 elseif(ANDROID AND ARA_USE_CURL)
     set(UTILITIES_LIBS
