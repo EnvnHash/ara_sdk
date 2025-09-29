07eaa5c26bd4c49c1cff5b099216a046a8dfe1e6
sven <hahne@zeitkunst.eu>
2025-09-29 15:04:48 -0300

    resdata symlink fix for arasdk module inclusion

--- Libraries/cmake/Modules/ResCompInclude.cmake
+++ Libraries/cmake/Modules/ResCompInclude.cmake
@@ -1,5 +1,9 @@
 include(${ARA_SDK_SOURCE_DIR}/Libraries/cmake/Modules/AraSdkMacros.cmake)
 
 if(${CMAKE_BUILD_TYPE} MATCHES Debug AND NOT ANDROID)
-    create_symlink(${CMAKE_SOURCE_DIR}/Assets/resdata ${CMAKE_CURRENT_BINARY_DIR}/resdata)
+    if (${ARA_IS_SUBPROJECT} AND (${CMAKE_CURRENT_BINARY_DIR} STREQUAL "${FETCHCONTENT_BASE_DIR}/ara_sdk-build"))
+        create_symlink(${CMAKE_SOURCE_DIR}/Assets/resdata ${CMAKE_BINARY_DIR}/resdata)
+    else ()
+        create_symlink(${CMAKE_SOURCE_DIR}/Assets/resdata ${CMAKE_CURRENT_BINARY_DIR}/resdata)
+    endif ()
 endif()
