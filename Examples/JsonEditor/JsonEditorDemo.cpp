afe5c6b85c9752b25d2339939f8dbbc8694e6065
sven <hahne@zeitkunst.eu>
2026-02-26 14:44:26 -0300

    jsoneditor demo WIP

---
+++ Examples/JsonEditor/JsonEditorDemo.cpp
@@ -0,0 +1,30 @@
+//
+// Created by user on 24.11.2020.
+//
+
+#include "JsonEditorDemo.h"
+#include <UIElements/Text/Label.h>
+
+#include "JsonEditor.h"
+
+using namespace std;
+using namespace ara;
+using namespace glm;
+
+void JsonEditorDemo::init(std::function<void(UINode&)>) {
+    UIApplication::init([](UINode& rootNode){
+        rootNode.setBackgroundColor(vec4(.1f, .1f, .1f, 1.f));
+        rootNode.setPadding(20.f);
+
+        auto& label = rootNode.push<Label>({ .size = ivec2{400,60} });
+        label.setFont("regular", 28, align::left, valign::top, vec4{1.f});
+        label.setText("Json Editor Demo");
+
+        auto& edit = rootNode.push<JsonEditor>(UINodePars{
+            .pos = ivec2{ 0, 40 },
+            .bgColor = vec4(.1f, .1f, .8f, 0.3f),
+        });
+        edit.setSize(1.f, -40);
+        edit.loadFile("test.json");
+    });
+}
---
+++ Examples/JsonEditor/JsonEditorDemo.h
@@ -0,0 +1,16 @@
+//
+// Created by user on 24.11.2020.
+//
+
+#pragma once
+
+#include <UIApplication.h>
+
+namespace ara {
+
+class JsonEditorDemo : public UIApplication {
+public:
+    void init(std::function<void(UINode&)> initCb) override;
+};
+
+}
