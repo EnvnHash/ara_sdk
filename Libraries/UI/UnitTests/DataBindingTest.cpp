c83a777e9d9f67f1a0800fd9179653aa41032c2f
sven <hahne@zeitkunst.eu>
2026-04-01 18:15:59 -0300

    NodeEdit std::string member variable test

--- Libraries/UI/UnitTests/DataBindingTest.cpp
+++ Libraries/UI/UnitTests/DataBindingTest.cpp
@@ -10,17 +10,16 @@ using namespace std;
 
 namespace ara::UiUnitTest::DataBindingTests {
 
+template <typename T>
 class TestNode : public Node {
 public:
     ARA_NODE_ADD_SERIALIZE_FUNCTIONS(Node, m_testValue)
-    TestNode() {
-        setTypeName<TestNode>();
-    }
-
-    int32_t m_testValue=0;
+    TestNode() { setTypeName<TestNode>(); }
+    T m_testValue{};
 };
 
-void addNodeEdit(const UIApplication &app, TestNode& node) {
+template <typename T>
+void addNodeEdit(const UIApplication &app, TestNode<T>& node) {
     auto& ne = app.getRootNode()->push<NodeEdit>();
     ne.setLineHeight(22);
     ne.setSpacing(10);
@@ -32,7 +31,7 @@ void addNodeEdit(const UIApplication &app, TestNode& node) {
 }
 
 TEST(UITest, NodeEditBasicTest) {
-    TestNode testNode;
+    TestNode<int32_t> testNode;
 
     appBody([&](const UIApplication &app) {
         addNodeEdit(app, testNode);
@@ -42,8 +41,8 @@ TEST(UITest, NodeEditBasicTest) {
     }, 400, 200);
 }
 
-TEST(UITest, NodeEditChangeValTest) {
-    TestNode testNode;
+TEST(UITest, NodeEditChangeFloatTest) {
+    TestNode<int32_t> testNode;
 
     appBody([&](const UIApplication &app) {
         addNodeEdit(app, testNode);
@@ -55,6 +54,28 @@ TEST(UITest, NodeEditChangeValTest) {
         compareFrameBufferToImage(filesystem::current_path() / "data_binding_change_val.png",
                                   app.getWinBase()->getWidth(), app.getWinBase()->getHeight(), 3);
         EXPECT_EQ(testNode.m_testValue, 1);
+    }, 400, 200);
+}
+
+TEST(UITest, NodeEditChangeStringTest) {
+    TestNode<std::string> testNode;
+    std::string testString = "hello";
+
+    appBody([&](const UIApplication &app) {
+        addNodeEdit(app, testNode);
+
+        const auto mainWin = app.getMainWindow();
+        mainWin->onMouseDownLeft(255, 11, false, false, false);
+        mainWin->onMouseUpLeft();
+        for (const auto &ch : testString) {
+            mainWin->onChar(ch);
+        }
+        mainWin->onKeyDown(ARA_KEY_ENTER, false, false, false);
+        mainWin->onKeyUp(ARA_KEY_ENTER, false, false, false);
+    }, [&](const UIApplication &app) {
+        compareFrameBufferToImage(filesystem::current_path() / "data_binding_change_string.png",
+                                  app.getWinBase()->getWidth(), app.getWinBase()->getHeight(), 3);
+        EXPECT_EQ(testNode.m_testValue, "hello");
     }, 400, 200);
 }
 
