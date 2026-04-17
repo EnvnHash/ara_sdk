408e4c6b39bff193e92e46728fef09485881912f
sven <hahne@zeitkunst.eu>
2026-04-17 14:11:15 -0400

    removing "name" from standard NodeMemberVariable exlusion

--- Libraries/UI/src/UIElements/DataBinding/NodeEdit.h
+++ Libraries/UI/src/UIElements/DataBinding/NodeEdit.h
@@ -43,7 +43,7 @@ private:
     int32_t     m_labelWidth = 180;
     arrange     m_editAlign{};
 
-    std::vector<std::string>                                    m_stdExcludeKeys { "name", "uuid", "typeName" };
+    std::vector<std::string>                                    m_stdExcludeKeys { "uuid", "typeName" };
     std::vector<std::string>                                    m_excludeKeys { m_stdExcludeKeys };
     std::unordered_map<std::string, VariableEditOption<>>       m_optPerKey;
     std::unordered_map<std::string, NodeMemberVariableEdit*>    m_variableEdits;
