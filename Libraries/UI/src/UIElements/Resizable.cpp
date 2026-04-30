da02109df277025493bdede174c17483e20035c9
sven <hahne@zeitkunst.eu>
2026-04-30 15:16:27 -0400

    Resizeable fixedAspect ratio

--- Libraries/UI/src/UIElements/Resizable.cpp
+++ Libraries/UI/src/UIElements/Resizable.cpp
@@ -77,11 +77,14 @@ void Resizable::initHandles() {
         }
         m_handles[i]->setResizeImage(this);
     }
+
+    setFixedAspect(m_fixedAspect);
 }
 
 void Resizable::setResizeStart(const Corner corner) {
     m_dragStartPos = getPos();
     m_dragStartSize = getSize();
+    m_dragStartAspect = m_dragStartSize.x / m_dragStartSize.y;
     m_dragCorner = corner;
     m_dragging = true;
 }
@@ -90,48 +93,89 @@ void Resizable::setResizeEnd() {
     m_dragging = false;
 }
 
-void Resizable::resizeFromCorner(const Corner corner, const vec2& movedPix) {
-    if (corner == Corner::bottomRight) {
-        setSize(max(m_minSize, ivec2(m_dragStartSize + movedPix)));
-    } else if (corner == Corner::topLeft) {
-        const auto newSize = ivec2(m_dragStartSize - movedPix);
-        setPos(min(ivec2(m_dragStartPos + movedPix), ivec2(m_dragStartPos  + m_dragStartSize - vec2(m_minSize))));
-        setSize(max(m_minSize, newSize));
-    } else if (corner == Corner::bottomLeft) {
-        const auto newSize = ivec2{ m_dragStartSize.x - movedPix.x, m_dragStartSize.y + movedPix.y };
-        setPos(getLeftDragX(movedPix.x), static_cast<int32_t>(m_dragStartPos.y));
-        setSize(max(m_minSize, newSize));
-    } else if (corner == Corner::topRight) {
-        const auto newSize = ivec2{ m_dragStartSize.x + movedPix.x, m_dragStartSize.y - movedPix.y };
-        setPos(static_cast<int32_t>(m_dragStartPos.x), getTopDragY(movedPix.y));
-        setSize(max(m_minSize, newSize));
-    } else if (corner == Corner::top) {
-        setSize(max(m_minSize, ivec2(m_dragStartSize.x, m_dragStartSize.y - movedPix.y)));
-        setPos(static_cast<int32_t>(m_dragStartPos.x), getTopDragY(movedPix.y));
-    } else if (corner == Corner::bottom) {
-        setSize(max(m_minSize, ivec2(m_dragStartSize.x, m_dragStartSize.y + movedPix.y)));
-    }  else if (corner == Corner::right) {
-        setSize(max(m_minSize, ivec2(m_dragStartSize.x + movedPix.x, m_dragStartSize.y)));
-    } else if (corner == Corner::left) {
-        setPos(getLeftDragX(movedPix.x), static_cast<int32_t>(m_dragStartPos.y));
-        setSize(max(m_minSize, ivec2(m_dragStartSize.x - movedPix.x, m_dragStartSize.y)));
-    } else if (corner == Corner::center) {
-        setPos(ivec2(m_dragStartPos + movedPix));
+void Resizable::setFixedAspect(const bool& val) {
+    m_fixedAspect = val;
+    for (auto& it : { Corner::left, Corner::right, Corner::top, Corner::bottom }) {
+        m_handles[static_cast<int32_t>(it)]->setVisibility(!val);
     }
+}
+
+void Resizable::resizeFromCorner(const Corner corner, const vec2& movedPix) {
+    ivec2 newSize = getNewSize(corner, movedPix);
+    if (m_fixedAspect) {
+        newSize.x = newSize.y * m_dragStartAspect;
+    }
+
+    setPos(getNewPos(corner, movedPix, newSize));
+    setSize(newSize);
 
     if (getSharedRes()) {
         getSharedRes()->reqRedraw();
     }
 }
 
-int32_t Resizable::getTopDragY(const int32_t moved) const {
-    return std::min(static_cast<int32_t>(m_dragStartPos.y + moved),
-                      static_cast<int32_t>(m_dragStartPos.y + m_dragStartSize.y) - m_minSize.y);
+ivec2 Resizable::getNewSize(const Corner corner, const vec2& movedPix) const {
+    static unordered_map<Corner, function<ivec2(const dragPar&)>> funcMap{
+        {Corner::bottomRight, [](const dragPar& dp) {
+            return max(dp.minSize, ivec2(dp.dragStartSize + dp.movedPix));
+        }}, {Corner::topLeft, [](const dragPar& dp) {
+            return max(dp.minSize, ivec2(dp.dragStartSize - dp.movedPix));
+        }}, {Corner::bottomLeft, [](const dragPar& dp) {
+            return max(dp.minSize, ivec2{dp.dragStartSize.x - dp.movedPix.x, dp.dragStartSize.y + dp.movedPix.y});
+        }}, {Corner::topRight, [](const dragPar& dp) {
+            return max(dp.minSize, ivec2{dp.dragStartSize.x + dp.movedPix.x, dp.dragStartSize.y - dp.movedPix.y});
+        }}, {Corner::top, [](const dragPar& dp) {
+            return max(dp.minSize, ivec2(dp.dragStartSize.x, dp.dragStartSize.y - dp.movedPix.y));
+        }}, {Corner::bottom, [](const dragPar& dp) {
+            return max(dp.minSize, ivec2(dp.dragStartSize.x, dp.dragStartSize.y + dp.movedPix.y));
+        }}, {Corner::right, [](const dragPar& dp) {
+            return max(dp.minSize, ivec2(dp.dragStartSize.x + dp.movedPix.x, dp.dragStartSize.y));
+        }}, {Corner::left, [](const dragPar& dp) {
+            return max(dp.minSize, ivec2(dp.dragStartSize.x - dp.movedPix.x, dp.dragStartSize.y));
+        }}, {Corner::center, [](const dragPar& dp) {
+            return dp.dragStartSize;
+        }}
+    };
+
+    const dragPar dp{movedPix, m_minSize, m_dragStartPos, m_dragStartSize, {}};
+    return funcMap[corner](dp);
 }
 
-int32_t Resizable::getLeftDragX(const int32_t moved) const {
-    return std::min(static_cast<int32_t>(m_dragStartPos.x + moved),
-                            static_cast<int32_t>(m_dragStartPos.x + m_dragStartSize.x) - m_minSize.x);
+ivec2 Resizable::getNewPos(const Corner corner, const vec2& movedPix, const vec2& newSize) const {
+    static unordered_map<Corner, function<ivec2(const dragPar&)>> funcMap{
+        {Corner::bottomRight, [](const dragPar& dp) {
+            return dp.dragStartPos;
+        }}, {Corner::topLeft, [](const dragPar& dp) {
+            return dp.dragStartPos - dp.newSize + dp.dragStartSize;
+        }}, {Corner::bottomLeft, [](const dragPar& dp) {
+            return ivec2{ dp.dragStartPos.x - dp.newSize.x + dp.dragStartSize.x, dp.dragStartPos.y };
+        }}, {Corner::topRight, [](const dragPar& dp) {
+            return ivec2{ dp.dragStartPos.x, dp.dragStartPos.y - dp.newSize.y + dp.dragStartSize.y };
+        }}, {Corner::top, [](const dragPar& dp) {
+            return ivec2{ dp.dragStartPos.x, dp.dragStartPos.y - dp.newSize.y + dp.dragStartSize.y };
+        }}, {Corner::bottom, [](const dragPar& dp) {
+            return ivec2{ dp.dragStartPos.x - dp.newSize.x + dp.dragStartSize.x, dp.dragStartPos.y };
+        }}, {Corner::right, [](const dragPar& dp) {
+            return dp.dragStartPos;
+        }}, {Corner::left, [](const dragPar& dp) {
+            return ivec2{ dp.dragStartPos.x - dp.newSize.x + dp.dragStartSize.x, dp.dragStartPos.y };
+        }}, {Corner::center, [](const dragPar& dp) {
+            return dp.dragStartPos + dp.movedPix;
+        }}
+    };
+
+    const dragPar dp{movedPix, m_minSize, m_dragStartPos, m_dragStartSize, newSize};
+    return funcMap[corner](dp);
+}
+
+int32_t Resizable::getTopDragY(const dragPar& dp) {
+    return std::min(static_cast<int32_t>(dp.dragStartPos.y + dp.movedPix.y),
+                      static_cast<int32_t>(dp.dragStartPos.y + dp.dragStartSize.y) - dp.minSize.y);
+}
+
+int32_t Resizable::getLeftDragX(const dragPar& dp) {
+    return std::min(static_cast<int32_t>(dp.dragStartPos.x + dp.movedPix.x),
+                            static_cast<int32_t>(dp.dragStartPos.x + dp.dragStartSize.x) - dp.minSize.x);
 }
 
 }
--- Libraries/UI/src/UIElements/Resizable.h
+++ Libraries/UI/src/UIElements/Resizable.h
@@ -11,27 +11,39 @@ namespace ara {
 
 class Resizable : public Div {
 public:
+    struct dragPar {
+        const glm::vec2& movedPix{};
+        const glm::ivec2& minSize{};
+        const glm::vec2& dragStartPos{};
+        const glm::vec2& dragStartSize{};
+        const glm::vec2& newSize{};
+    };
+
     Resizable();
 
     void init() override;
     void initHandles();
     void setResizeStart(ResizableHandle::Corner corner);
     void setResizeEnd();
-    void resizeFromCorner(ResizableHandle::Corner corner, const glm::vec2& movedPix);
-
+    void setFixedAspect(const bool& val);
     void setMinSize(const glm::vec2& size) { m_minSize = size; }
 
 private:
-    int32_t getTopDragY(int32_t moved) const;
-    int32_t getLeftDragX(int32_t moved) const;
+    void resizeFromCorner(ResizableHandle::Corner corner, const glm::vec2& movedPix);
+    glm::ivec2 getNewSize(ResizableHandle::Corner corner, const glm::vec2& movedPix) const;
+    glm::ivec2 getNewPos(ResizableHandle::Corner corner, const glm::vec2& movedPix, const glm::vec2& newSize) const;
+    static int32_t getTopDragY(const dragPar& dp);
+    static int32_t getLeftDragX(const dragPar& dp);
 
     ResizableHandle::Corner m_dragCorner = ResizableHandle::Corner::bottomRight;
     glm::vec2               m_dragStartMouseParentRel{};
     glm::vec2               m_dragStartPos{};
     glm::vec2               m_dragStartSize{};
+    float                   m_dragStartAspect{};
     glm::ivec2              m_minSize = {40, 40 };
     int32_t                 m_handleSize = 12;
     bool                    m_dragging = false;
+    bool                    m_fixedAspect = false;
 
     std::array<ResizableHandle*, static_cast<int>(ResizableHandle::Corner::size)> m_handles{};
 
